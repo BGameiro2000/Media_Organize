84656f6b28e7a51e65f6490246cea6713b53d47b
BGameiro76 <33392539+BGameiro76@users.noreply.github.com>
2018-03-28 17:23:22 +0100

    tries to automate the raw strings
    3 ways to automate the process
    all failed

--- GUI/menu.py
+++ GUI/menu.py
@@ -40,7 +40,9 @@ ttk.Label(window, text="Which media would you like to access?").grid(row=0)
 # Radiobutton global variables (list)
 Apps = ["Name displayed", "Música", "Fotos"]
 Paths = ["Path to the app", "C:\\Program Files (x86)\\MAGIX\\MP3 deluxe 19\\MP3deluxe.exe", "F:\\VLC\\vlc.exe"]
-#rPaths = [re.compile(p) for p in Paths]
+#rPaths = [re.escape(p) for p in Paths]
+#rPaths = ["%r"%p for p in Paths]
+#rPaths = "%r"%Paths
 rPaths = ["Path to the app", r"C:\\Program Files (x86)\\MAGIX\\MP3 deluxe 19\\MP3deluxe.exe", r"F:\\VLC\\vlc.exe"]
 AppsPath = dict(zip(Apps, rPaths))
 

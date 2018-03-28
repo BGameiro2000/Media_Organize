0efe930ba7689f8a589eb74fdacaf3d0a0d3f591
BGameiro76 <33392539+BGameiro76@users.noreply.github.com>
2018-03-28 20:35:13 +0100

    initial commit
    comments
    this program will allow the user to change the apps and their paths in order to use them in menu.py

---
+++ GUI/ChangeApp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This code is under Apache License 2.0
+Written by B_Gameiro (Bernardo Bernardino Gameiro)
+More in 
+  SoloLearn: https://www.sololearn.com/Profile/8198571
+  GitHub: https://github.com/BGameiro76
+  Repl.it: https://repl.it/@B_Gameiro
+Program to change the apps and their paths in order to use menu.py
+GUI in Python 3
+Using Tkinter
+"""

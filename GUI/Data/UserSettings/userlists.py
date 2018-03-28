f37071e3b71fc1436b77ab73be0b36e9f8d1452b
BGameiro76 <33392539+BGameiro76@users.noreply.github.com>
2018-03-28 20:25:19 +0100

    lists that can be changed
    here are the lists that the user may change
    the first has the names that will be displayed
    the second has the paths to those apps
    
    previously in the main file

---
+++ GUI/Data/UserSettings/userlists.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This code is under Apache License 2.0
+Written by B_Gameiro (Bernardo Bernardino Gameiro)
+More in 
+  SoloLearn: https://www.sololearn.com/Profile/8198571
+  GitHub: https://github.com/BGameiro76
+  Repl.it: https://repl.it/@B_Gameiro
+Lists to use in Media Organization Menu project
+GUI in Python 3
+Using Tkinter
+"""
+
+#======================
+# lists
+#======================
+Apps = ["Name displayed", "Música", "Fotos"]
+Paths = ["Path to the app", "C:\\Program Files (x86)\\MAGIX\\MP3 deluxe 19\\MP3deluxe.exe", "F:\\VLC\\vlc.exe"]
